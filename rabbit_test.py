f6d61b57b2a067897621766d697d235552af5bb7
Victor Manuel Ninahuanca Ayala <victor.ninahuanca@agetic.gob.bo>
2025-02-17 14:42:31 -0400

    feat: actualizar credenciales clickhouse

--- rabbit_test.py
+++ rabbit_test.py
@@ -5,24 +5,24 @@ from datetime import datetime
 from urllib.parse import urlparse
 
 # Configuración de RabbitMQ
-RABBITMQ_URL = 'amqp://3DG5TI8cSct9mz84:1tbhh6v3kidlHODHaoAS1LArDBx.IuqS@junction.proxy.rlwy.net:46601'
+RABBITMQ_URL = "amqp://3DG5TI8cSct9mz84:1tbhh6v3kidlHODHaoAS1LArDBx.IuqS@junction.proxy.rlwy.net:46601"
 # amqp://3DG5TI8cSct9mz84:1tbhh6v3kidlHODHaoAS1LArDBx.IuqS@rabbitmq.railway.internal:5672
-RABBITMQ_QUEUE = 'clickhouse_queue'
+RABBITMQ_QUEUE = "clickhouse_queue"
 
 # Extraer host, puerto y credenciales de la URL de RabbitMQ
 parsed_url = urlparse(RABBITMQ_URL)
-RABBITMQ_HOST = 'junction.proxy.rlwy.net'
+RABBITMQ_HOST = "junction.proxy.rlwy.net"
 RABBITMQ_PORT = 46601
-RABBITMQ_USER = '3DG5TI8cSct9mz84'
-RABBITMQ_PASSWORD = '1tbhh6v3kidlHODHaoAS1LArDBx.IuqS'
+RABBITMQ_USER = "3DG5TI8cSct9mz84"
+RABBITMQ_PASSWORD = "1tbhh6v3kidlHODHaoAS1LArDBx.IuqS"
 
 # Configuración de ClickHouse
-CLICKHOUSE_HOST = 'clickhouse-production-0732.up.railway.app'
+CLICKHOUSE_HOST = "clickhouse-4bj6-production.up.railway.app"
 CLICKHOUSE_PORT = 443
-CLICKHOUSE_USER = 'clickhouse'
-CLICKHOUSE_PASSWORD = 'zW00wtHXvt8wKcdCApITp0s9fMHCsfuH'
-CLICKHOUSE_DATABASE = 'railway'
-CLICKHOUSE_TABLE = 'bitacora'
+CLICKHOUSE_USER = "clickhouse"
+CLICKHOUSE_PASSWORD = "IhaRmopenWm5lCav8huJkdmPF9bgApVN"
+CLICKHOUSE_DATABASE = "railway"
+CLICKHOUSE_TABLE = "bitacora"
 
 # Conectar a ClickHouse
 try:
@@ -32,7 +32,7 @@ try:
         username=CLICKHOUSE_USER,
         password=CLICKHOUSE_PASSWORD,
         database=CLICKHOUSE_DATABASE,
-        secure=True 
+        secure=True,
     )
     print("Conexión exitosa a ClickHouse!")
 except Exception as e:
@@ -74,57 +74,63 @@ ORDER BY idTransaccion;
 def callback(ch, method, properties, body):
     try:
         # Deserializar el mensaje
-        message = json.loads(body.decode('utf-8'))
-        bitacora_data = message.get('bitacora')
-        personas_data = message.get('personas', [])
+        message = json.loads(body.decode("utf-8"))
+        bitacora_data = message.get("bitacora")
+        personas_data = message.get("personas", [])
+
+        print("Bitácora recibida:", json.dumps(bitacora_data, indent=4))
 
         # Convertir la cadena de fecha y hora a un objeto datetime
-        fechayHora_str = bitacora_data['fechayHora']
-        fechayHora_dt = datetime.strptime(fechayHora_str, '%Y-%m-%dT%H:%M:%SZ')
+        fechayHora_str = bitacora_data["fechayHora"]
+        fechayHora_dt = datetime.strptime(fechayHora_str, "%Y-%m-%dT%H:%M:%SZ")
 
         # Insertar en la tabla de bitácora
         clickhouse_client.insert(
             CLICKHOUSE_TABLE,
-            [(
-                bitacora_data['idTransaccion'],
-                bitacora_data['idSolicitud'],
-                bitacora_data['codTramite'],
-                bitacora_data['nroTramite'],
-                bitacora_data['usuarioConsumidor'],
-                bitacora_data['entidadConsumidora'],
-                bitacora_data['sistemaCosumidor'],
-                bitacora_data['sistemaPublicador'],
-                bitacora_data['servicio'],
-                bitacora_data['entidadPublicadora'],
-                fechayHora_dt, 
-                bitacora_data['estado'],
-                bitacora_data['codHTTP']
-            )]
+            [
+                (
+                    bitacora_data["idTransaccion"],
+                    bitacora_data["idSolicitud"],
+                    bitacora_data["codTramite"],
+                    bitacora_data["nroTramite"],
+                    bitacora_data["usuarioConsumidor"],
+                    bitacora_data["entidadConsumidora"],
+                    bitacora_data["sistemaConsumidor"],
+                    bitacora_data["sistemaPublicador"],
+                    bitacora_data["servicio"],
+                    bitacora_data["entidadPublicadora"],
+                    fechayHora_dt,
+                    bitacora_data["estado"],
+                    bitacora_data["codHTTP"],
+                )
+            ],
         )
 
         # Insertar en la tabla de personas
         if personas_data:
             clickhouse_client.insert(
-                'personas',
-                [(
-                    bitacora_data['idTransaccion'],
-                    persona['tipoId'],
-                    persona['valorId'],
-                    persona['complemento']
-                ) for persona in personas_data]
+                "personas",
+                [
+                    (
+                        bitacora_data["idTransaccion"],
+                        persona["tipoId"],
+                        persona["valorId"],
+                        persona["complemento"],
+                    )
+                    for persona in personas_data
+                ],
             )
 
         print("Datos insertados en ClickHouse")
     except Exception as e:
         print(f"Error al procesar el mensaje: {e}")
 
+
 # Conectar a RabbitMQ
 try:
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
     connection_params = pika.ConnectionParameters(
-        host=RABBITMQ_HOST,
-        port=RABBITMQ_PORT,
-        credentials=credentials
+        host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=credentials
     )
     connection = pika.BlockingConnection(connection_params)
     channel = connection.channel()
